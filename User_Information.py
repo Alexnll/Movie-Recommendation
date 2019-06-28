d6ee18ade51bcdf1bdda246af5d3907c1c960842
Alexnll <czxalexnl@gmail.com>
2019-06-28 16:06:12 +0800

    删除User——Correlation.py,将其功能整哈到User_Information.py中，并完成后者的大致框架

--- User_Information.py
+++ User_Information.py
@@ -6,18 +6,60 @@
 import numpy as np
 import pandas as pd
 
+# 数据集地址
+path_20m = '.\\dataset\\ml-20m\\'
+path_small = '.\\dataset\\ml-latest-small\\'
+
+# 用户信息保存地址
+path_user = '.\\new_user\\'
+
 class user:
+    __user_Id = None
+    __rating_list = None   # 储存用户对某部电影的评分
 
     # 初始化用户的账户iD，密码，以及一个空的字典列表
-    def __init__(self, userId, password):
-        self.user_Id = userId
-        self.password = password
-        self.rating_list = {}          # 储存用户对某部电影的评分
+    def __init__(self, login_or_sign):
+        # 注册账户
+        if login_or_sign == 'login':
+            self.__user_Id = __account_generated()
+            self.__rating_list = {} 
+        # 登录账户
+        elif login_or_sign == 'sign':
+            read_Id = input('Please print your user Id: ')
+            self.__sign_in(read_Id)
 
-    def
+    # 初始化一个新的userId, 并生成一个以Id为名字的csv保存信息
+    def __account_generated(self):
+        pass
 
+    # 登陆时读取该用户的电影列表
+    def __read_dict(self, Id):
+        pass
+    
+    # 登录
+    def __sign_in(self, Id):
+        # 读取账户Id
+        self.__user_Id = Id
+        # 读取电影列表
+        self.__rating_list
 
+    # 搜索电影，或加入star列表
+    def movie_search(self, movie_name):
+        pass
 
-# 初始化一个新的userId，并随机生成一个password
-def  account_generated():
-    pass
+    # 为电影打分
+    def movie_rating(self, movie_name, rate):
+        pass
+
+    # 查看自己以打分或以star但未打分的电影
+    def see_dict(self):
+        pass
+
+    # 根据自己的打分电影列表进行电影推荐
+    # 基于用户的协同过滤
+    def recom_movie(self):
+        pass
+
+if __name__ == '__main__':
+    trial_login = user('login')
+    trial_sign = user('sign')
